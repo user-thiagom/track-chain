39baafbd2d1275394fb8bcbb13bccea9e0ce2488
Thiago Mateus <thiagomateus123@hotmail.com>
2022-06-03 00:00:25 -0300

    Mudança no spinner

--- src/Pages/TrackingPage/index.js
+++ src/Pages/TrackingPage/index.js
@@ -1,4 +1,4 @@
-import { Anchor, Avatar, Box, Button, DataTable, Footer, Grid, Nav, Sidebar, Spinner, Text, Tip } from 'grommet'
+import { Avatar, Box, Button, Grid, Image, Nav, Sidebar, Spinner, Text, Tip } from 'grommet'
 import { Location, Clipboard } from 'grommet-icons'
 import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
@@ -10,8 +10,8 @@ import './index.css'
 function TrackingPage() {
   const [contracts, setContracts] = useState([])
   const [loading, setLoading] = useState(true)
-  const [show,setShow] = useState(false)
-  const [id,setId] = useState(0)
+  const [show, setShow] = useState(false)
+  const [id, setId] = useState(0)
 
   useEffect(() => {
     setTimeout(() => {
@@ -29,10 +29,6 @@ function TrackingPage() {
     console.log(datum)
   }
 
-  if (loading) {
-    return <Spinner message="Carregando nesse carai" />
-  }
-
   const teste = contracts.map((con) => {
     return {
       id: con.id,
@@ -41,15 +37,30 @@ function TrackingPage() {
     }
   })
 
+  if (loading) {
+    return (
+      <Box
+      align='center'
+      justify='center'
+      height='100vh'
+      animation='pulse'
+      background='dark-1'
+      >
+        <Text size='2xl' margin='medium'>Espere um momento</Text>
+        <Spinner message="Carregando nesse carai" color='accent-1' size='large' />
+      </Box>
+    )
+  }
+
   return (
     <>
       <Grid
-      rows={['100vh','auto']}
-      columns={['xsmall', 'auto']}
-      areas={[
-        { name: 'sidebar', start: [0, 0], end: [0, 1] },
-        { name: 'main', start: [1, 0], end: [1, 1] }
-      ]}
+        rows={['100vh', 'auto']}
+        columns={['xsmall', 'auto']}
+        areas={[
+          { name: 'sidebar', start: [0, 0], end: [0, 1] },
+          { name: 'main', start: [1, 0], end: [1, 1] }
+        ]}
       >
         <Sidebar background='brand' header={<Avatar src='//s.gravatar.com/avatar/b7fb138d53ba0f573212ccce38a7c43b?s=80' />} gridArea='sidebar' align='center'>
           <Nav>
@@ -61,10 +72,10 @@ function TrackingPage() {
             </Tip>
           </Nav>
         </Sidebar>
-        <Tabela dados={teste} gridArea='main' clickRow={click}/>
+        <Tabela dados={teste} gridArea='main' clickRow={click} />
 
         {show && (
-          <TrackingOverlay id={id} handle={setShow}/>
+          <TrackingOverlay id={id} handle={setShow} />
         )}
 
       </Grid>
