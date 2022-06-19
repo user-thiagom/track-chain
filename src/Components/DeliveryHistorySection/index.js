2f249b190c617f58d4346ecb4fc68d22ac276ecc
Thiago Mateus <thiagomateus123@hotmail.com>
2022-06-19 12:41:17 -0300

    Barra de rolagem na tela de rastreamento

--- src/Components/DeliveryHistorySection/index.js
+++ src/Components/DeliveryHistorySection/index.js
@@ -1,13 +1,22 @@
+import { Box } from 'grommet'
 import React from 'react'
 import CardTrackingInfo from '../CardTrackingInfo/'
 
 function index({contract}) {
   return (
-    <>
+    <Box
+    wrap
+    direction='row'
+    justify='center'
+    align='center'
+    fill='horizontal'
+    overflow='auto'
+    pad={{bottom:'small'}}
+    >
         {contract.map((history) => {
             return <CardTrackingInfo data={history} />
         })}
-    </>
+    </Box>
   )
 }
 
